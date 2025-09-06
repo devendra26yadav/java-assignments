0e9a98b4b9bea5b88b2000873838e9bfd26f046b
Devendra Yadav <devraj@Devendras-MacBook-Air.local>
2025-09-05 22:49:35 -0500

    Bug fixed

--- Sum.java
+++ Sum.java
@@ -3,4 +3,12 @@ public class Sum{
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public static int findSum(int[] arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+
+    }
 }
